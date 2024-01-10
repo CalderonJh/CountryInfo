5fbabfd281780322a9489e585ed4bffa143ff4e7
Jhon Calderon <jhoncalderon20174@gmail.com>
2024-01-10 08:53:07 -0500

    not found country page

---
+++ src/app/countries/components/country-not-found/country-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-not-found',
+  templateUrl: './country-not-found.component.html',
+  styleUrl: './country-not-found.component.css'
+})
+export class CountryNotFoundComponent {
+
+}
---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-country-table',
+  templateUrl: './country-table.component.html',
+  styleUrl: './country-table.component.css',
+})
+export class CountryTableComponent {}
