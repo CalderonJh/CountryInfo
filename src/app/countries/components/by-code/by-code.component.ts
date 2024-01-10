a2674b7ca9f1a89d28f11fb57593d8849ecafa3e
Jhon Calderon <jhoncalderon20174@gmail.com>
2024-01-10 14:13:14 -0500

    by code component

---
+++ src/app/countries/components/by-code/by-code.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-by-code',
+  templateUrl: './by-code.component.html',
+  styleUrl: './by-code.component.css'
+})
+export class ByCodeComponent {
+
+}
